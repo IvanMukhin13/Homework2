6351ac45ddd64addd030ba415e1e326f5f8a927e
IvanMukhin13 <loyalty26@yandex.ru>
2024-05-28 12:56:36 +0300

    Add files via upload

---
+++ Homework2.py
@@ -0,0 +1,9 @@
+first_name = 'Курс: Python'
+second_name = 'Всего задач'
+second_count = 12
+third_name = 'затрачено часов'
+third_count = 1.5
+fourth_name = 'среднее время выполнения'
+fourth_count = third_count / second_count
+
+print(first_name,':',second_name,':',second_count,':',third_name,':',third_count, ':', fourth_name,':', fourth_count)
